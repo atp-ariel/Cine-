4b1685e3fb8c0e0351c2373caa058c9ea227f5d2
ArielTriana <usich37@gmail.com>
2021-05-30 01:18:34 -0400

    Funciona el fomrulario

--- src/Cine+/DomainLayer/Identity/SignUpModel.cs
+++ src/Cine+/DomainLayer/Identity/SignUpModel.cs
@@ -9,7 +9,19 @@ namespace DomainLayer.Identity
         public string Email { get; set; }
 
         [Required]
+        [DataType(DataType.Text)]
+        public string Username {get; set;}
+
+        [Required]
         [DataType(DataType.Password, ErrorMessage = "Contraseña inválida u omitida")]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmedPassword {get; set;}
+
+        public bool MatchPasswords(){
+            return Password == ConfirmedPassword;
+        }
     }
 }
