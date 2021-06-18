1a0f46fe1792ebec666cba4c4d21526de1480a41
Chains <aylinalvarezsantos@gmail.com>
2021-06-18 18:57:17 -0400

    Update manual de usuario

--- src/Cine+/ServiceLayer/Statistics/TicketSalesStatisticsYear.cs
+++ src/Cine+/ServiceLayer/Statistics/TicketSalesStatisticsYear.cs
@@ -30,7 +30,7 @@ namespace ServiceLayer.Statistics
             DateTime end = start.AddMonths(1);
 
             List<TicketPurchase> tickets = context.TicketPurchase.ToList();
-            int count = context.TicketPurchase.Count(x => (x.BatchScheduleStartTime.CompareTo(start) >= 0 && x.BatchScheduleEndTime.CompareTo(end) <= 0));
+            int count = context.TicketPurchase.Count(x => (x.TimeReserve.CompareTo(start) >= 0 && x.TimeReserve.CompareTo(end) <= 0));
             ticketsSoldDict.Add(start.Month, count);
             ticketsSold = count;
 
@@ -39,7 +39,7 @@ namespace ServiceLayer.Statistics
             {
                 end = end.AddMonths(1);
                 start = start.AddMonths(1);
-                int count_ = context.TicketPurchase.Count(x => x.BatchScheduleStartTime.CompareTo(start) > 0 && x.BatchScheduleEndTime.CompareTo(end) <= 0);
+                int count_ = context.TicketPurchase.Count(x => x.TimeReserve.CompareTo(start) > 0 && x.TimeReserve.CompareTo(end) <= 0);
                 ticketsSoldDict.Add(start.Month, count_);
                 ticketsSold += count_;
 
