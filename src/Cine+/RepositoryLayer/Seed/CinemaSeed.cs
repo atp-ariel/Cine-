f5f875c2badea61ef88cfc2561b302ee5d8e9f13
ArielTriana <usich37@gmail.com>
2021-06-18 06:32:04 -0400

    Fix issues

---
+++ src/Cine+/RepositoryLayer/Seed/CinemaSeed.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using DomainLayer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RepositoryLayer.Seed
+{
+    public class CinemaSeed : ISeed
+    {
+        private Cinema[] cinemas = new[] { 
+            new Cinema(){Id=1, Capacity=100 },
+            new Cinema(){Id=2, Capacity=200},
+            new Cinema(){Id=3, Capacity=50}
+        };
+        public void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+                context.Database.Migrate();
+
+            foreach(var cinema in cinemas)
+            {
+                cinema.Seats = new List<Seat>();
+                for (int i = 0; i < cinema.Capacity; i++)
+                    cinema.Seats.Add(new Seat() { Id = i, Cinema = cinema, CinemaId = cinema.Id });
+            }
+            foreach (var cinema in cinemas)
+                if (!context.Cinema.Contains(cinema))
+                    context.Add(cinema);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ src/Cine+/RepositoryLayer/Seed/DiscountSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using DomainLayer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace RepositoryLayer.Seed
+{
+    public class DiscountSeed : ISeed
+    {
+        private Discount[] discounts = new[]
+        {
+            new Discount(){Id = 1, DiscountedMoney=0.20f, Name="FEU" },
+            new Discount(){Id = 2, DiscountedMoney=1f, Name="Tercera Edad"}
+        };
+        public void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+                context.Database.Migrate();
+
+            foreach (var discount in discounts)
+                if (!context.Discount.Contains(discount))
+                    context.Add(discount);
+            context.SaveChanges();
+        }
+    }
+}
